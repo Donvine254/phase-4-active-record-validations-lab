7d39bda60652c9993bc8cb7c1158baabdaf74f33
donvine mugendi <donvinemugendi@gmail.com>
2023-08-15 22:15:21 +0300

    completed lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true, length: { minimum: 250 }
+  validates :summary, presence: true, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+  validate :click_baity
+
+  def click_baity
+    clickbait_words = ["Won't Believe", 'Secret', 'Top [number]', 'Guess']
+    
+    if title.nil? || !clickbait_words.any? { |word| title.include?(word) }
+      errors.add(:title, 'not clickbait-y enough!')
+    end
+  end
+  
 end
